12badad9517bbd0f5fbb1486355319c9e24a53b7
hyrumk <hyrumk@kaist.ac.kr>
2021-11-13 22:20:29 +0900

    Initial Commit

---
+++ app/src/main/java/com/cs492/skincam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs492.skincam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
