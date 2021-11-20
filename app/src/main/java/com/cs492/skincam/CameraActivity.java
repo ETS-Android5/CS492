4f6152efd67cb15c3e3c15ba7139b684783a6919
HyojunKim <junstar98@naver.com>
2021-11-21 00:48:17 +0900

    Camera
    JOT

--- app/src/main/java/com/cs492/skincam/CameraActivity.java
+++ app/src/main/java/com/cs492/skincam/CameraActivity.java
@@ -1,19 +1,54 @@
 package com.cs492.skincam;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
+import android.os.Build;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class CameraActivity extends AppCompatActivity {
+    ImageView photo_view;
+    final static int TAKE_PICTURE = 1;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        photo_view = (ImageView) findViewById(R.id.photo_view);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, TAKE_PICTURE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case TAKE_PICTURE:
+                if (resultCode == RESULT_OK && data.hasExtra("data")) {
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    if (bitmap != null) {
+                        photo_view.setImageBitmap(bitmap);
+
+                    }
+                }
+                break;
         }
     }
 }
--- app/src/main/java/com/cs492/skincam/MainActivity.java
+++ app/src/main/java/com/cs492/skincam/MainActivity.java
@@ -1,71 +1,59 @@
 package com.cs492.skincam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.gms.vision.CameraSource;
 
 public class MainActivity extends AppCompatActivity {
 
+    final private static String TAG = "GILBOMI";
+
     Button btn_cam;
     Button btn_gallery;
-    TextView textView;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.InitializeView();
-        this.SetListener();
-    }
-
-    public void InitializeView(){
         btn_cam = (Button) findViewById(R.id.btn_camera);
         btn_gallery = (Button) findViewById(R.id.btn_gallery);
-        textView = (TextView) findViewById(R.id.text_sex);
 
-    }
-    public void SetListener(){
         btn_cam.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                dispatchTakePictureIntent();
-
+                switch (view.getId()){
+                    case R.id.btn_camera:
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                                Log.d(TAG, "권한 설정 완료");
+                            }
+                            else{
+                                Log.d(TAG, "권한 설정 요청");
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                            }
+                        }
+                        Intent photoIntent = new Intent(getApplicationContext(), CameraActivity.class);
+                        startActivity(photoIntent);
+                        break;
+                }
             }
         });
-
-        btn_gallery.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                textView.setTextColor(Color.GREEN);
-            }
-        });
     }
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//            imageView.setImageBitmap(imageBitmap);
-        }
-    }
-
 
 }
