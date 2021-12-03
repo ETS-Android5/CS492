e58dc8016b7b3555543846dec23287b4bbb682e0
hyrumk <hyrumk@kaist.ac.kr>
2021-12-03 19:15:53 +0900

    Fix permission

--- app/src/main/java/com/cs492/skincam/MainActivity.java
+++ app/src/main/java/com/cs492/skincam/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cs492.skincam;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -20,6 +21,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.gms.vision.CameraSource;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     final private static String TAG = "CAMERA";
@@ -27,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
     Button btn_cam;
     Button btn_gallery;
 
+    final int RequestPermissionID = 1001;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA, // Camera
+            Manifest.permission.READ_EXTERNAL_STORAGE};  // Write External
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RequestPermissionID) {
+            boolean check_result = true;
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                }
+            }
+            if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,20 +60,18 @@ public class MainActivity extends AppCompatActivity {
         btn_cam = (Button) findViewById(R.id.btn_camera);
         btn_gallery = (Button) findViewById(R.id.btn_gallery);
 
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    REQUIRED_PERMISSIONS,
+                    RequestPermissionID);
+        }
+
         btn_cam.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 switch (view.getId()){
                     case R.id.btn_camera:
-                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
-                                Log.d(TAG, "권한 설정 완료");
-                            }
-                            else{
-                                Log.d(TAG, "권한 설정 요청");
-                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                            }
-                        }
                         Intent photoIntent = new Intent(getApplicationContext(), CameraActivity.class);
                         startActivity(photoIntent);
                         break;
@@ -57,12 +80,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d(TAG, "onRequestPermissionsResult");
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
-        }
-    }
 }
