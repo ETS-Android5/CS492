8db0734f8ba90ea515bd3214ace28d1d9957d0b3
HyojunKim <junstar98@naver.com>
2021-12-05 00:16:42 +0900

    delete cameraActivity & put it in MainActivity

--- app/src/main/java/com/cs492/skincam/MainActivity.java
+++ app/src/main/java/com/cs492/skincam/MainActivity.java
@@ -4,15 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.ImageDecoder;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -21,9 +25,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.google.android.gms.vision.CameraSource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView photo_view;
+    Uri photoURI;
+
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     final private static String TAG = "CAMERA";
 
@@ -72,8 +86,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 switch (view.getId()){
                     case R.id.btn_camera:
-                        Intent photoIntent = new Intent(getApplicationContext(), CameraActivity.class);
-                        startActivity(photoIntent);
+                        dispatchTakePictureIntent();
                         break;
                 }
             }
@@ -91,6 +104,77 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.cs492.skincam.fileprovider",
+                        photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = null;
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            try{
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                    bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), photoURI));
+                }else{
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if(bitmap != null) {
+                float scale = (float) (1024 / (float) (bitmap.getWidth()));
+                int image_w = (int) (bitmap.getWidth() * scale);
+                int image_h = (int) (bitmap.getHeight() * scale);
+                Bitmap resize = Bitmap.createScaledBitmap(bitmap, image_w, image_h, true);
+                resize.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("image", byteArray);
+
+                startActivity(intent);
+            }
+
+        }
     }
 
 }
