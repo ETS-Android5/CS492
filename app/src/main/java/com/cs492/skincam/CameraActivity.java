17127c73a2f8e1804d14e0596d6d38daf0a5a857
HyojunKim <junstar98@naver.com>
2021-12-04 21:26:51 +0900

    fix camera bug & camera picture to result activity

--- app/src/main/java/com/cs492/skincam/CameraActivity.java
+++ app/src/main/java/com/cs492/skincam/CameraActivity.java
@@ -6,8 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
 import android.graphics.ImageDecoder;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -21,11 +23,14 @@ import android.widget.ImageView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import org.pytorch.torchvision.TensorImageUtils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
@@ -33,63 +38,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class CameraActivity extends AppCompatActivity {
     ImageView photo_view;
-    final static int TAKE_PICTURE = 1;
+    Uri photoURI;
 
-    String mCurrentPhotoPath;
-    final static int REQUEST_TAKE_PHOTO = 1;
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-
         photo_view = (ImageView) findViewById(R.id.photo_view);
 
         dispatchTakePictureIntent();
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        try {
-            switch (requestCode) {
-                case REQUEST_TAKE_PHOTO:
-                    if (resultCode == RESULT_OK){
-                        File file = new File(mCurrentPhotoPath);
-                        Bitmap bitmap;
-
-                        if (Build.VERSION.SDK_INT >= 29){
-                            ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), Uri.fromFile(file));
-                            try{
-                                bitmap = ImageDecoder.decodeBitmap(source);
-                                if (bitmap != null){
-                                    photo_view.setImageBitmap(bitmap);
-                                }
-                            }catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } else{
-                            try{
-                                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
-                                if(bitmap != null){
-                                    photo_view.setImageBitmap(bitmap);
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    break;
-            }
-        } catch (Exception error){
-            error.printStackTrace();
-        }
-    }
 
     private File createImageFile() throws IOException{
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -100,42 +66,67 @@ public class CameraActivity extends AppCompatActivity {
                 ".jpg",
                 storageDir
         );
-
-        mCurrentPhotoPath = image.getAbsolutePath();
+        currentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
+        Log.d("sex", String.valueOf(takePictureIntent.resolveActivity(getPackageManager()) != null));
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
             File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.cs492.skincam.fileprovider",
+                        photoFile);
 
-            try { photoFile = createImageFile();}
-            catch (IOException ex){}
-            if(photoFile != null){
-                Uri photoURI = FileProvider.getUriForFile(this, "com.cs492.skincam.fileprovider", photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
     }
 
-    public static String assetFilePath(Context context, String assetName) throws IOException {
-        File file = new File(context.getFilesDir(), assetName);
-        if (file.exists() && file.length() > 0) {
-            return file.getAbsolutePath();
-        }
 
-        try (InputStream is = context.getAssets().open(assetName)) {
-            try (OutputStream os = new FileOutputStream(file)) {
-                byte[] buffer = new byte[4 * 1024];
-                int read;
-                while ((read = is.read(buffer)) != -1) {
-                    os.write(buffer, 0, read);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = null;
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            try{
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                    bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), photoURI));
+                }else{
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), photoURI);
                 }
-                os.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return file.getAbsolutePath();
+
+            if(bitmap != null) {
+                float scale = (float) (1024 / (float) (bitmap.getWidth()));
+                int image_w = (int) (bitmap.getWidth() * scale);
+                int image_h = (int) (bitmap.getHeight() * scale);
+                Bitmap resize = Bitmap.createScaledBitmap(bitmap, image_w, image_h, true);
+                resize.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent intent = new Intent(CameraActivity.this, ResultActivity.class);
+                intent.putExtra("image", byteArray);
+
+                startActivity(intent);
+            }
+
         }
     }
 }
